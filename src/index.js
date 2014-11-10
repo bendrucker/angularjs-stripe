ebfae7bedb0e5db781194d0cbe6797d72278d2bb
Ben Drucker <bvdrucker@gmail.com>
2014-11-10 16:41:35 -0500

    Remove unnecessary index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-module.exports = require('./module');
+'use strict';
+
+var angular = require('angular');
+
+angular.module('angular-stripe', [])
+  .provider('stripe', require('./provider'));
+
+module.exports = 'angular-stripe';
--- src/module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var angular = require('angular');
-
-angular.module('angular-stripe', [])
-  .provider('stripe', require('./provider'));
-
-module.exports = 'angular-stripe';
