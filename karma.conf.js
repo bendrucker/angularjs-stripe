4180f625c9de7238feda214363d9f785bb008631
Ben Drucker <bvdrucker@gmail.com>
2014-11-10 17:17:07 -0500

    cleanup karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,83 +1,30 @@
-// Karma configuration
-// Generated on Tue May 20 2014 23:34:58 GMT-0400 (EDT)
+'use strict';
 
 module.exports = function (config) {
   config.set({
-
-  // base path that will be used to resolve all patterns (eg. files, exclude)
-  basePath: '',
-
-
-  // frameworks to use
-  // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-  frameworks: ['browserify', 'mocha', 'chai-sinon'],
-
-
-  // list of files / patterns to load in the browser
-  browserify: {
-    debug: true,
-    transform: ['browserify-istanbul', 'browserify-shim']
-  },
-
-
-  // list of files to exclude
-  exclude: [
-    
-  ],
-
-  files: [
-    'node_modules/angular/angular.js',
-    'node_modules/angular-mocks/angular-mocks.js',
-    'components/stripe/index.js',
-    'node_modules/es5-shim/es5-shim.js',
-    'node_modules/chai-as-promised/lib/chai-as-promised.js',
-    'test/*.js'
-  ],
-
-
-  // preprocess matching files before serving them to the browser
-  // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-  preprocessors: {
-    'test/*.js': 'browserify'
-  },
-
-
-  // test results reporter to use
-  // possible values: 'dots', 'progress'
-  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-  reporters: ['progress', 'coverage'],
-
-  coverageReporter: {
-    type: 'lcovonly',
-    dir: 'coverage',
-    subdir: '.'
-  },
-
-
-  // web server port
-  port: 9876,
-
-
-  // enable / disable colors in the output (reporters and logs)
-  colors: true,
-
-
-  // level of logging
-  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-  logLevel: config.LOG_INFO,
-
-
-  // enable / disable watching file and executing tests whenever any file changes
-  autoWatch: true,
-
-
-  // start these browsers
-  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-  browsers: ['PhantomJS'],
-
-
-  // Continuous Integration mode
-  // if true, Karma captures browsers, runs the tests and exits
-  singleRun: false
+    frameworks: ['browserify', 'mocha', 'chai-sinon'],
+    browserify: {
+      debug: true,
+      transform: ['browserify-istanbul', 'browserify-shim']
+    },
+    files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'components/stripe/index.js',
+      'node_modules/es5-shim/es5-shim.js',
+      'node_modules/chai-as-promised/lib/chai-as-promised.js',
+      'test/*.js'
+    ],
+    preprocessors: {
+      'test/*.js': 'browserify'
+    },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type: 'lcovonly',
+      dir: 'coverage',
+      subdir: '.'
+    },
+    browsers: ['PhantomJS'],
+    singleRun: false
   });
 };
