79dd5f6a9651f8c343b84ebe68216d5d52ec1869
Ben Drucker <bvdrucker@gmail.com>
2014-11-10 16:47:48 -0500

    Refactor service for better readability

--- src/service.js
+++ src/service.js
@@ -3,39 +3,35 @@
 var angular = require('angular');
 var Stripe  = require('stripe');
 
-var internals = {};
-
-internals.promisify = function (receiver, method) {
-  var $q = internals.$q;
-  return  function (data) {
-    var deferred = $q.defer();
-    receiver[method](data, function (status, response) {
-      if (response.error) return deferred.reject(response.error);
-      return deferred.resolve(response);
-    });
-    return deferred.promise;
-  };
-};
-
-internals.wrap = function (source, options) {
-  var angularStripe = {};
-  angular.forEach(options, function (methods, receiver) {
-    var destination = angularStripe[receiver] = {};
-    receiver = Stripe[receiver];
-    /* istanbul ignore else */
-    if (methods.promisify) angular.forEach(methods.promisify, function (method) {
-      destination[method] = internals.promisify(receiver, method);
-    });
-    if (methods.reference) angular.forEach(methods.reference, function (method) {
-      destination[method] = receiver[method];
-    });
-  });
-  return angularStripe;
-};
-
 module.exports = function ($q) {
-  internals.$q = $q;
-  return internals.wrap(Stripe, {
+
+  function promisify (receiver, method) {
+    return  function (data) {
+      var deferred = $q.defer();
+      receiver[method](data, function (status, response) {
+        if (response.error) return deferred.reject(response.error);
+        return deferred.resolve(response);
+      });
+      return deferred.promise;
+    };
+  }
+
+  function wrap (source, options) {
+    var angularStripe = {};
+    angular.forEach(options, function (methods, receiver) {
+      var destination = angularStripe[receiver] = {};
+      receiver = Stripe[receiver];
+      angular.forEach(methods.promisify, function (method) {
+        destination[method] = promisify(receiver, method);
+      });
+      angular.forEach(methods.reference, function (method) {
+        destination[method] = receiver[method];
+      });
+    });
+    return angularStripe;
+  }
+
+  return wrap(Stripe, {
     card: {
       reference: ['validateCardNumber', 'validateExpiry', 'validateCVC', 'cardType'],
       promisify: ['createToken']
