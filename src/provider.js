9397f0e9751de576a82f746246c5eea9285d93d1
Ben Drucker <bvdrucker@gmail.com>
2015-04-17 10:01:35 -0400

    Throw when Stripe.js isn't available
    This can't be easily tested since the Stripe constant is set up when the
    module loads
    
    Closes #8

--- src/provider.js
+++ src/provider.js
@@ -5,6 +5,7 @@ export default stripeProvider
 
 stripeProvider.$inject = ['Stripe']
 function stripeProvider (Stripe) {
+  if (!Stripe) throw new Error('Stripe must be available as window.Stripe')
   this.setPublishableKey = Stripe.setPublishableKey
   this.$get = service
 }
