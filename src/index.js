b672b384cd4621d11458f3a3f5bba4779dc442ff
Ben Drucker <bvdrucker@gmail.com>
2015-05-01 18:21:58 -0400

    Ensure $q is a function, otherwise demand angular-q-constructor
    Prevents future ocurrences of #14

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 'use strict'
 
 import angular from 'angular'
+import assertQCtor from 'angular-assert-q-constructor'
 import provider from './provider'
 import Stripe from 'stripe'
 
-export default angular.module('angular-stripe', [])
-  .constant('Stripe', Stripe)
-  .provider('stripe', provider)
-  .name
+export default angular.module('angular-stripe', [
+  assertQCtor
+])
+.constant('Stripe', Stripe)
+.provider('stripe', provider)
+.run(verifyQ)
+.name
+
+verifyQ.$inject = ['assertQConstructor']
+function verifyQ (assertQConstructor) {
+  assertQConstructor('angular-stripe: For Angular <= 1.2 support, first load https://github.com/bendrucker/angular-q-constructor')
+}
